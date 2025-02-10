e03b759815ad1820a003a2ced7fd3f6c51fdb975
Sergio Rodriguez <sergio.a.rodriguez@du.edu>
2025-02-09 23:45:46 -0700

    ccsds packets added

--- README.md
+++ README.md
@@ -1,2 +1,106 @@
-# Voyager1
-Emulation for a school peoject
+# VOYAGER1-Project
+This is for a class, I'm emulating voyager ones data collection and compression algorithm. The decompression algorithims are freely available online at https://planetarydata.jpl.nasa.gov/img/data/vg2-u-iss-2-edr-v1.0/vg_0001/software/ 
+
+## Notes
+cdcomp.c needs to be ported for modern systems.
+
+
+
+
+## Important Links
+Stats on the sensors 
+https://pds-ppi.igpp.ucla.edu/mission/Voyager/Imaging_Science_Subsystem_-_Wide_Angle
+
+
+Camera Stats
+
+Instrument Information
+======================
+Instrument Id : ISSW
+Instrument Host Id : VG2
+Pi Pds User Id : BASMITH
+Naif Data Set Id : UNK
+Instrument Name : IMAGING SCIENCE SUBSYSTEM - WIDE
+ANGLE
+Instrument Type : VIDICON CAMERA
+Build Date : 1976-12-17
+Instrument Mass : 13.300000
+Instrument Length : 0.550000
+Instrument Width : 0.200000
+Instrument Height : 0.200000
+Instrument Serial Number : SN04
+Instrument Manufacturer Name : JET PROPULSION LABORATORY
+
+Instrument Description
+======================
+The Voyager Imaging Science Subsystem (ISS) is a modified
+version of the slow scan vidicon camera designs that were used
+in the earlier Mariner flights. The system consists of two
+cameras, a high resolution Narrow Angle (NA) camera and a lower
+resolution, more sensitive Wide Angle (WA) camera. Unlike the
+other on board instruments, operation of the cameras is not
+autonomous, but is controlled by an imaging parameter table
+residing in one of the spacecraft computers, the Flight Data
+Subsystem (FDS) (Science and Mission Systems Handbook, 1987,
+JPL D-498, an internal JPL document available from JPL vellum
+files).
+
+The original mission was to Jupiter and Saturn. Voyager
+surpassed expectations and went on to encounter Uranus and
+Neptune. As the Voyager mission progressed the objects
+photographed were further from the sun so they appeared more
+faint even though longer exposures were used. As the Voyager
+spacecrafts' distance from the Earth increases, the
+telecommunications capability at each encounter decreases. The
+difference in capabilities from the Jupiter and Saturn
+encounters and that at Uranus and Neptune was considerable.
+The reduced telecommunications capability limits the number of
+data modes that imaging can use. Because of the diminished
+brightness of the objects being photographed, longer exposure
+times were used, many beyond the stated maximum of 15.360
+seconds. Longer exposure times were all 48-second increments
+added to the maximum. In addition, the camera was slewed in
+order to avoid smeared imaging. The light flood state (on/off)
+was independent of the instrument mode.
+
+
+
+Description
+-----------
+The sensor used in the Voyager Imaging Science Subsystem ISS
+camera system is a 25mm diameter magnetic deflection vidicon
+(number B41-003, General Electro-dynamics Co.). The vidicon
+storage surface (target) is selenium sulphur and can store a
+high resolution (1500 TV lines) picture for over 100 s at
+room temperature. The active image area on the target is
+11.14 x 11.14 mm. Each frame consists of 800 lines with 800
+picture elements (pixels) per line, i.e., 1 pixel = 14
+microns. One frame requires 48 s for electronic readout. In
+addition to the normal frame readout of 48 s (1:1), four
+extended frame-time modes of 2:1, 3:1, 5:1, and 10:1 are
+available by command. Following readout, light flooding is
+used to remove any residual image that might remain from the
+previous frame. At the end of light flooding, 14 erase
+frames are used to stabilize and prepare the vidicon target
+for the next exposure sequence (VGR ISS Calibration Report,
+1978, an internal JPL document available from JPL vellum
+files).
+
+
+Description
+-----------
+The Imaging Science Subsystem (ISS) electronics consist of
+the vidicon support circuits and the signal chain. The
+vidicon support circuits are the vertical and horizontal
+sweep circuits, and the various power supplies for the
+vidicon filament, and the focus and alignment coils. The
+signal chain consists of the analog signal amplifiers,
+bandpass filters, and an eight bit analog-to-digital
+converter. The digital output is sent to the Flight Data
+Subsystem (FDS) for editing.
+
+
+
+CCSDS packet standards
+https://public.ccsds.org/Pubs/133x0b2e2.pdf
+https://docs.ccsdspy.org/en/latest/user-guide/ccsds.html 
--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include "SD.h"
 #include "SPI.h"
 
+#include <stdio.h>
+#include <stdint.h>
+
 #define CAMERA_MODEL_XIAO_ESP32S3 // Has PSRAM
 
 #include "camera_pins.h"
@@ -38,38 +41,12 @@ void setup()
     config.pin_pwdn = PWDN_GPIO_NUM;
     config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
-    config.frame_size = FRAMESIZE_SVGA;
-    config.pixel_format = PIXFORMAT_JPEG; // for streaming
+    config.frame_size = FRAMESIZE_SVGA;     // 600x800 frame size
+    config.pixel_format = PIXFORMAT_RGB565; // uncompressed format
     config.grab_mode = CAMERA_GRAB_WHEN_EMPTY;
     config.fb_location = CAMERA_FB_IN_PSRAM;
     config.jpeg_quality = 12;
     config.fb_count = 1;
-
-    // if PSRAM IC present, init with UXGA resolution and higher JPEG quality
-    //                      for larger pre-allocated frame buffer.
-    if (config.pixel_format == PIXFORMAT_JPEG)
-    {
-        if (psramFound())
-        {
-            config.jpeg_quality = 10;
-            config.fb_count = 2;
-            config.grab_mode = CAMERA_GRAB_LATEST;
-        }
-        else
-        {
-            // Limit the frame size when PSRAM is not available
-            config.frame_size = FRAMESIZE_SVGA;
-            config.fb_location = CAMERA_FB_IN_DRAM;
-        }
-    }
-    else
-    {
-        // Best option for face detection/recognition
-        config.frame_size = FRAMESIZE_240X240;
-#if CONFIG_IDF_TARGET_ESP32S3
-        config.fb_count = 2;
-#endif
-    }
 
     // camera init
     esp_err_t err = esp_camera_init(&config);
@@ -84,6 +61,68 @@ void setup()
     Serial.println("Photos will begin in one minute, please be ready.");
 }
 
+/*Structs*/
+
+#pragma pack(push, 1) // Ensure no padding is added to the struct
+
+typedef struct
+{
+    uint8_t version;          // 3 bits version
+    uint8_t type;             // 1 bit type 0 for telemetry 1 for telecommand
+    uint8_t secondary_header; // 1 bit for true false
+    uint8_t apid;             // [8 bits APID LSB]
+    uint8_t sequence_flags;   // [2 bits Sequence Flags] | [14 bits Sequence Count MSB]
+    uint8_t sequence_count;   // [8 bits Sequence Count LSB]
+    uint16_t packet_length;   // [16 bits Packet Length] (Payload size - 1)
+} CCSDS_PrimaryHeader;
+
+#pragma pack(pop) // Restore default struct padding
+
+#pragma pack(push, 1)
+typedef struct
+{
+    uint32_t timestamp; // Example: 32-bit timestamp
+    uint16_t data_id;   // Identifier for specific data type
+} CCSDS_SecondaryHeader;
+#pragma pack(pop)
+
+#pragma pack(push, 1)
+typedef struct
+{
+    CCSDS_PrimaryHeader primaryHeader;
+#ifdef INCLUDE_SECONDARY_HEADER
+    CCSDS_SecondaryHeader secondaryHeader; // Optional
+#endif
+    uint8_t data[]; // Payload (variable length)
+} CCSDS_Packet;
+#pragma pack(pop)
+
+/*Function Prototypes*/
+
+void createPrimaryHeader_image(CCSDS_Packet *packet, size_t data_len)
+{
+    if (packet == nullptr)
+    {
+        return;
+    }
+    packet->primaryHeader.version = 0b000;
+    packet->primaryHeader.type = 0;
+    packet->primaryHeader.secondary_header = 0;
+    // create a camera apid code
+    packet->primaryHeader.apid = 0x7FF;
+    // to bits describing firt or continuation
+    packet->primaryHeader.sequence_flags = 0b10;
+    // count the number of packets increments with each Packet 32 is 32nd packet
+    packet->primaryHeader.sequence_count = 0;
+    // set to the size of one frame
+    packet->primaryHeader.packet_length = data_len;
+}
+
+void sendData(CCSDS_Packet *packet)
+{
+    Serial.write(packet->data,packet->primaryHeader.packet_length);
+}
+
 void loop()
 {
     delay(1000); // Wait for a second before taking the next photo
@@ -94,15 +133,21 @@ void loop()
 
     if (fb)
     {
-        Serial.println("Captured frame:");
-        Serial.print("Height: ");
-        Serial.println(fb->height);
-        Serial.print("Width: ");
-        Serial.println(fb->width);
-        Serial.print("Length: ");
-        Serial.println(fb->len);
 
-        // Process the frame here
+        // Create CCSDS_Packet
+        CCSDS_Packet *ccsds_packet = (CCSDS_Packet *)malloc(sizeof(CCSDS_Packet) + fb->len);
+
+        // Create the primary header for the packet
+        createPrimaryHeader_image(ccsds_packet,fb->len);
+
+        // Copy the image data to the CCSDS packet payload
+        memcpy(ccsds_packet->data, fb->buf, fb->len);
+
+        // Send data
+        sendData(ccsds_packet);
+
+        // Frees
+        free(ccsds_packet);
 
         // Return the frame buffer after use
         esp_camera_fb_return(fb);
@@ -115,3 +160,34 @@ void loop()
 
 /*Code bibliography for setting up camera
 https://github.com/limengdu/SeeedStudio-XIAO-ESP32S3-Sense-camera/blob/main/take_photos/take_photos.ino*/
+
+/* Notes
+Serial.println("Captured frame:");
+        Serial.print("Height: ");
+        Serial.println(fb->height);
+        Serial.print("Width: ");
+        Serial.println(fb->width);
+        Serial.print("Length: ");
+        Serial.println(fb->len);
+        // Process the frame here, such as reading pixel data
+        // For example, print the RGB565 pixel data
+        uint16_t *pixels = (uint16_t *)fb->buf;
+
+for (int i = 0; i < fb->width * fb->height; i++)
+        {
+            uint16_t pixel = pixels[i];
+            uint8_t r = (pixel >> 11) & 0x1F; // Red component (5 bits)
+            uint8_t g = (pixel >> 5) & 0x3F;  // Green component (6 bits)
+            uint8_t b = pixel & 0x1F;         // Blue component (5 bits)
+
+            // Print RGB values for a few pixels (e.g., first pixel)
+            if (i == 0)
+            {
+                Serial.print("First pixel R=");
+                Serial.print(r);
+                Serial.print(" G=");
+                Serial.print(g);
+                Serial.print(" B=");
+                Serial.println(b);
+            }
+        }*/
