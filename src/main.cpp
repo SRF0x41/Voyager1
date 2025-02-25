e3610439265766bf5d677e21235926201768855b
Sergio Rodriguez <sergio.a.rodriguez@du.edu>
2025-02-25 15:36:34 -0700

    diverging branches

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,20 @@
-#include <BLEDevice.h>
-#include <BLEUtils.h>
-#include <BLE2902.h>
-#include <BLEServer.h>
+
 #include <Wire.h>
 #include <Arduino.h>
 
-int main()
-{
+typedef struct {
+  // Valid commands 
+  
+}
 
-  return 0;
+void setup() {
+    Serial.begin(115200);  // Start serial communication at 115200 baud rate
+}
+
+void loop() {
+    if (Serial.available() > 0) {  // Check if data is available
+        String receivedData = Serial.readString();  // Read incoming string
+        Serial.print("Received: ");
+        Serial.println(receivedData);
+    }
 }
